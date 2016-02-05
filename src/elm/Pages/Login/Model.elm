3f2bf79b27b0b30d0f561f856cf97dfd04a98507
John Verrone <john.verrone@gmail.com>
2016-02-05 10:03:03 -0500

    initial commit, decoupled front-end from server-side

---
+++ src/elm/Pages/Login/Model.elm
@@ -0,0 +1,37 @@
+module Pages.Login.Model where
+
+import Http exposing (Error)
+
+type alias AccessToken = String
+
+type alias LoginForm =
+    { username : String
+    , password : String
+    }
+
+type UserMessage
+    = None
+    | Error String
+
+type Status
+    = Init
+    | Fetching
+    | Fetched
+    | HttpError Http.Error
+
+type alias Model =
+    { accessToken : AccessToken
+    , hasAccessTokenInStorage : Bool
+    , loginForm : LoginForm
+    , status : Status
+    , userMessage : UserMessage
+    }
+
+initialModel : Model
+initialModel =
+    { accessToken = ""
+    , hasAccessTokenInStorage = True
+    , loginForm = LoginForm "test" "123"
+    , status = Init
+    , userMessage = None
+    }
